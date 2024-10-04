afc53e0f9bc6089e5cf45d9d8222bb27c1ff9098
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-10-04 06:17:20 +0530

    Backend Class-3 code added

---
+++ BackendClass-3/controllers/deleteTodo.js
@@ -0,0 +1,23 @@
+//import model
+const Todo = require("../models/Todo");
+
+//define route handler
+
+exports.deleteTodo = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Todo.findByIdAndDelete(id);
+
+        res.json({
+            success: true,
+            message : "Todo deleted successfully"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            error: err.message,
+            message: "Server error",
+          });
+    }
+}
